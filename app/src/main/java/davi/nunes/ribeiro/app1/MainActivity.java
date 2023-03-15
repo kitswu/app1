35305d2987d6e4f2aaf4c5ae4d582794ff6cf3eb
20211TIIMI0028 <davinrgamer@gmail.com>
2023-03-15 09:08:07 -0300

    comentários explicativos

--- app/src/main/java/davi/nunes/ribeiro/app1/MainActivity.java
+++ app/src/main/java/davi/nunes/ribeiro/app1/MainActivity.java
@@ -15,15 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnEnviar = findViewById(R.id.btnEnviar);
-        Intent i = new Intent(MainActivity.this, NextActivity.class);
-        btnEnviar.setOnClickListener(new View.OnClickListener() {
+        Button btnEnviar = findViewById(R.id.btnEnviar); //busca o botão na tela pelo id
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() { //adiciona evento quando o botão é clicado
             @Override
-            public void onClick(View view) {
-                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
-                String textoDigitado = etDigiteAqui.getText().toString();
-                i.putExtra("texto", textoDigitado);
-                startActivity(i);
+            public void onClick(View view) { //método que roda quando o botão é ativado
+                Intent i = new Intent(MainActivity.this, NextActivity.class); //cria nova intent, que passa a intenção de navegar de um lugar pra outro, no caso, de uma tela (activity) para outra.
+                EditText etDigiteAqui = findViewById(R.id.etDigiteAqui); //busca o edit text pelo id
+                String textoDigitado = etDigiteAqui.getText().toString(); //pega o texto do edit text e converte pra string
+                i.putExtra("texto", textoDigitado); //cria no dicionário do intent uma chave "texto" e associa com o texto digitado
+                startActivity(i); //muda para a tela indicada na criação da intent
             }
         });
 
--- app/src/main/java/davi/nunes/ribeiro/app1/NextActivity.java
+++ app/src/main/java/davi/nunes/ribeiro/app1/NextActivity.java
@@ -12,9 +12,9 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
-        Intent i = getIntent();
-        String textoDigitado = i.getStringExtra("texto");
-        TextView tvTexto = findViewById(R.id.tvTexto);
-        tvTexto.setText(textoDigitado);
+        Intent i = getIntent(); //recebe as informações da intent passada
+        String textoDigitado = i.getStringExtra("texto"); //pega a chave "texto" criada na última activity
+        TextView tvTexto = findViewById(R.id.tvTexto); //busca o textview pelo id
+        tvTexto.setText(textoDigitado); //coloca o texto digitado no textview
     }
 }
